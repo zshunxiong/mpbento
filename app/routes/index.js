0e27bccfd6522a1b0d2e9886da3952192a2c02a0
teoh <teoh@program.com.tw>
2022-04-28 10:09:34 +0800

    Rename project

---
+++ app/routes/index.js
@@ -0,0 +1,4 @@
+module.exports = app => {
+  // API 所有路徑寫這裡
+  require("./tutorial.routes")(app);
+};
--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // DB 
 const db = require("./app/models");
 // 強制重置的指令
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-db.sequelize.sync();
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+// db.sequelize.sync();
 
 // Api route
-require("./app/routes/tutorial.routes")(app);
+require("./app/routes")(app);
 
 // Vue index route
 const path = __dirname + '/app/html/';
