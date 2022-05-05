c4ce1ae66f88546921697a8ea172e218e3eaacf8
seon <zshunxiong@gmail.com>
2022-05-05 09:32:29 +0800

    Fix db connection setting

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   dialect: "mysql",
   pool: {
     max: 5,
